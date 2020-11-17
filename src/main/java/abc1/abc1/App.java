daf997d4c4e5105b0ec096778676fc6b3cd48e5f
fasih uddin <uddin24fasih@yahoo.com>
2020-11-17 09:10:45 -0500

    first commit....

---
+++ src/main/java/abc1/abc1/App.java
@@ -0,0 +1,16 @@
+package abc1.abc1;
+
+import org.junit.Test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	@Test
+   public void print() {
+	   System.out.println("hello world");
+   }
+  
+}
